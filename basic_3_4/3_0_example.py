90ccff52561fa708bde0e4f873bfd11bd9566675
AnastasiaKolo <kolodochko@gmail.com>
2021-09-14 22:39:18 +0300

    2 B tasks finished

--- basic_3_4/3_0_example.py
+++ basic_3_4/3_0_example.py
@@ -32,5 +32,46 @@ def delete(x):
             return
 
 # Пример 2.
+# Дана последовательность положительных чисел длиной N
+# и число Х
+# Нужно найти 2 различных числа А и В из последовательности,
+# таких что А + В = Х или вернуть пару 0, 0 если таких чисел нет
+
+# решение за O(N^2)
+def twotermswithsumx(nums, x):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums [j] == x:
+                return nums[i], nums [j]
+    return 0, 0
+
+# решение за O(N)
+def twotermswithsumx(nums, x):
+    prevnums = set()
+    for nownum in nums:
+        if x - nownum in prevnums:
+            return nownum, x - nownum
+        prevnums.add(nownum)
+    return 0, 0
+
 
 # Пример 3.
+# Дан словарь из N слов,
+# длина каждого из которых не превосходит К
+# В записи каждого из M слов текста (каждое длиной до К)
+# может быть пропущена одна буква
+# Для каждого слова сказать, входит ли оно
+# (возможно, с одной пропущенной буквой) в словарь
+
+# Решение за O(NK + M)
+
+def wordsindict(dictionary, text):
+    goodwords = set(dictionary)
+    # теперь добавим возможные варианты с пропущенной буквой
+    for word in dictionary:
+        for delpos in range(len(word)):
+            goodwords.add(word[:delpos] + word[delpos+1:])
+    ans = []
+    for word in text:
+        ans.append(word in goodwords)
+    return ans
