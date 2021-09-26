c42ad725589877ceda8c25631edcd35c36b7ae6b
AnastasiaKolo <kolodochko@gmail.com>
2021-09-26 21:54:24 +0300

    6B D finished

--- basic_5_6/6B_E_K_intervals.py
+++ basic_5_6/6B_E_K_intervals.py
@@ -8,3 +8,31 @@
 # Формат вывода
 # Минимальное такое ℓ, что точки можно покрыть
 # k отрезками длины ℓ.
+
+def lbinsearch(l, r, check, checkparams):
+    while l < r:
+        m = (l + r) // 2
+        if check(m, checkparams):
+            r = m
+        else:
+            l = m + 1
+    return l
+
+def checkptreesinforest(days, params):
+    a, k, b, m, x = params
+    dmitr = a * days - a * (days // k)
+    fedr = b * days - b * (days // m)
+    return dmitr + fedr >= x
+
+
+a, k, b, m, x = map(int, input().split())
+
+
+
+'''
+input
+6 2
+1 2 3 9 8 7
+output
+2
+'''
