eede66c82fb710773bd24cb8258f21b7d703fea3
AnastasiaKolo <kolodochko@gmail.com>
2021-09-30 23:13:09 +0300

    7B C tried

--- basic_7_8/7B_C_min_cover.py
+++ basic_7_8/7B_C_min_cover.py
@@ -28,7 +28,30 @@ def is_covered(m, events):
             return False
     return True
 
+def best_section(point, idx_start, sections):
+    i = idx_start
+    bestsection = (0, 0)
+    maxright = 0
+    while (i < len(sections)) and (sections[i][0] <= point) and (sections[i][1] >= point):
+        if sections[i][1] - point > maxright:
+            bestsection = sections[i]
+            maxright = sections[i][1] - point
+        i += 1
+    return bestsection
 
+def is_covered_multi(m, sections):
+    current_point = 0
+    ans = []
+    i = 0
+    while (current_point <= m) and (i < len(sections)):
+        max_right_section = best_section(current_point, i, sections)
+        if max_right_section != (0, 0):
+            ans.append(max_right_section)
+            current_point = max_right_section[1]
+        elif max_right_section == (0, 0) and (current_point < m):
+            return []
+        i += 1
+    return ans
 
 with open('input.txt') as f:
     m = int(f.readline())
@@ -42,23 +65,24 @@ with open('input.txt') as f:
             # в массив событий сразу добавляем события с признаком
             # начало отрезка -1
             # конец отрезка 1
-            events.append((l, -1, r))
-            events.append((r, 1, l))
-        elif (r <= 0) or (l >= m):
+            # events.append((l, -1, r))
+            # events.append((r, 1, l))
+        elif (r < 0) or (l >= m):
             # совсем ненужные отрезки пропускаем
             pass
         else:
             eof = True
-events.sort()
-sections.sort()
-ans = is_covered(m, events)
-print(ans)
-# if ans:
-#     print(len(ans))
-#     for rec in ans:
-#         print(ans[0] + ' ' + ans[1])
-# else:
-#     print('No solution')
+# events.sort(key=lambda x: (x[0], x[1], -x[2]))
+sections.sort(key=lambda x: (x[0], -x[1]))
+# ans = is_covered(m, events)
+ans2 = is_covered_multi(m, sections)
+# print(ans)
+if ans2:
+    print(len(ans2))
+    for rec in ans2:
+        print(str(rec[0]) + ' ' + str(rec[1]))
+else:
+    print('No solution')
 
 '''
 input
